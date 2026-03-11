0400a9af45d44ab1560bd9eb0cd062cd01e34099
GTriXy <GTriXy@users.noreply.github.com>
2026-03-11 20:34:31 +0800

    fixbug and add fluid

---
+++ kubejs/startup_scripts/fluid/fluid.js
@@ -0,0 +1,8 @@
+StartupEvents.registry("fluid", (event) => {
+    event.create("shan:phenol", "thin")
+		.tint(0x593319)
+    event.create("shan:phenolic_resin", "thick")
+		.tint(0xbda27b)
+    event.create("shan:frmaldehyde", "thick")
+		.tint(0xc4d1d1)
+})
