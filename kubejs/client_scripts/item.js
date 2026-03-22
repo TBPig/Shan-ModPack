d3e8e996d68adcf7179cbf81d7250139a9b8bb0e
GTriXy <GTriXy@users.noreply.github.com>
2026-03-22 18:08:43 +0800

    tooltips

---
+++ kubejs/client_scripts/item.js
@@ -0,0 +1,57 @@
+ItemEvents.modifyTooltips(event => {
+    event.add('minecraft:iron_ingot', 'Fe'),
+    event.add('minecraft:gold_ingot', 'Au'),
+    event.add('minecraft:copper_ingot', 'Cu'),
+    event.add('minecraft:redstone', 'Si(FeS₂)₅CrAl₂O₃Hg₃'),
+    event.add('minecraft:glowstone_dust', 'Au(Si(FeS₂)₅CrAl₂O₃Hg₃)₂'),
+    event.add('minecraft:emerald', 'Be₃Al₂Si₆O₁₈'),
+    event.add('minecraft:lapis_lazuli', '(Na,Ca)₈Al₆Si₆O₂₄(S,SO₄)'),
+    event.add('minecraft:quartz', 'SiO₂'),
+    event.add('minecraft:diamond', 'C'),
+    event.add('minecraft:coal', 'C'),
+    event.add('anvilcraft:earth_core_shard', 'Ø'),
+    event.add('anvilcraft:cursed_gold_ingot', 'Au₂₄ϴ'),
+    event.add('anvilcraft:royal_steel_ingot', 'Fe₂C·BeAl₂Si₉O₂₁'),
+    event.add('anvilcraft:frost_metal_ingot', '(Fe₂C·BeAl₂Si₉O₂₁)₂ψ'),
+    event.add('anvilcraft:neutronium_ingot', 'Σ'),
+    event.add('anvilcraft:transcendium_ingot', 'Λ(Nb*Pu/U+Σ)'),
+    event.add('anvilcraft:ember_metal_ingot', 'Nb*Ø'),
+    event.add('minecraft:heart_of_the_sea', '∇'),
+    event.add('anvilcraft:prismarine_cluster', '∇₂ψ₃'),
+    event.add('anvilcraft:titanium_ingot', 'Ti'),
+    event.add('anvilcraft:tungsten_ingot', 'W'),
+    event.add('anvilcraft:tin_ingot', 'Sn'),
+    event.add('anvilcraft:uranium_ingot', 'U'),
+    event.add('anvilcraft:bronze_ingot', 'Cu₂Sn'),
+    event.add('anvilcraft:brass_ingot', 'Cu₂Zn'),
+    event.add('anvilcraft:silver_ingot', 'Ag'),
+    event.add('anvilcraft:lead_ingot', 'Pb'),
+    event.add('anvilcraft:zinc_ingot', 'Zn'),
+    event.add('anvilcraft:magnet_ingot', 'Fe'),
+    event.add('anvilcraft:plutonium_ingot', 'Pu'),
+    event.add('mekanism:ingot_osmium', 'Os'),
+    event.add('mekanism:fluorite_gem', 'CaF₂'),
+    event.add('minecraft:netherite_ingot', 'Nb'),
+    event.add('mekanism:ingot_steel', 'Fe₅C'),
+    event.add('mekanism:ingot_refined_glowstone', 'Au(Si(FeS₂)₅CrAl₂O₃Hg₃)₂·Os'),
+    event.add('mekanism:ingot_refined_obsidian', 'SiO₂(MgFe)₂Al₄O₈·Os'),
+    event.add('minecraft:charcoal', 'C')
+})
+/*
+数字	下标	上标	
+0	₀	⁰	
+1	₁	¹	
+2	₂	²	
+3	₃	³	
+4	₄	⁴	
+5	₅	⁵	
+6	₆	⁶	
+7	₇	⁷	
+8	₈	⁸	
+9	₉	⁹	
++	₊	⁺	
+-	₋	⁻	
+(	₍	⁽	
+)	₎	⁾	
+
+*/
